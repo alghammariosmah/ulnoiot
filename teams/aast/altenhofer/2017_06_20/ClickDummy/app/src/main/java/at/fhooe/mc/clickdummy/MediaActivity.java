10c3a97e02a7e8989352ff60c9915d44a7e3e91c
chris altenhofer <chrisaltenhofer@gmail.com>
2017-06-23 13:06:19 +0200

    added webview + refresh button (show ipcam images). teamlog update with scenario. personal log.

--- teams/aast/altenhofer/2017_06_20/ClickDummy/app/src/main/java/at/fhooe/mc/clickdummy/MediaActivity.java
+++ teams/aast/altenhofer/2017_06_20/ClickDummy/app/src/main/java/at/fhooe/mc/clickdummy/MediaActivity.java
@@ -3,18 +3,28 @@ package at.fhooe.mc.clickdummy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 
 /**
  * Created by christian on 20.06.2017.
  */
 
 public class MediaActivity extends AppCompatActivity {
+
+    private String ipAddress;
+
+    @BindView(R.id.webview)
+    WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,11 @@ public class MediaActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Media");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle bundle = getIntent().getExtras();
+        ipAddress = bundle.getString(MainActivity.IP_EXTRA);
+
+        setUpWebView("http://" + ipAddress + ":8081");
     }
 
     @Override
@@ -56,5 +71,19 @@ public class MediaActivity extends AppCompatActivity {
             msgText = "media 1 pause";
         }
         MainActivity.publish(msgText);
+    }
+
+    @OnClick(R.id.btn_refresh)
+    public void refresh() {
+        Toast.makeText(this, "Refresh...", Toast.LENGTH_SHORT).show();
+        webView.reload();
+    }
+
+    private void setUpWebView(String url) {
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        setTitle(url);
+        webView.loadUrl(url);
     }
 }
