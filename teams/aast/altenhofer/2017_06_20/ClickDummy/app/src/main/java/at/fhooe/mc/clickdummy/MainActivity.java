46024cbac75e69a6ac878ca987762288d20c8d0e
chris altenhofer <chrisaltenhofer@gmail.com>
2017-06-23 11:24:38 +0200

    added your friends home stuff to teamlog. added ip field to client application.

--- teams/aast/altenhofer/2017_06_20/ClickDummy/app/src/main/java/at/fhooe/mc/clickdummy/MainActivity.java
+++ teams/aast/altenhofer/2017_06_20/ClickDummy/app/src/main/java/at/fhooe/mc/clickdummy/MainActivity.java
@@ -6,12 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,12 +27,24 @@ public class MainActivity extends AppCompatActivity {
     private static MqttClient client;
     private static String TOPIC = "client/endpoint";
     private static int QOS = 2;
-    private String broker = "tcp://192.168.1.104:1883"; // tcp: is necessary
+    private String broker;
     private String clientId = "android_client";
     private MemoryPersistence persistence = new MemoryPersistence();
 
+    // ip address regex
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String IPADDRESS_PATTERN =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.server_ip)
+    EditText ipEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +56,10 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Overview");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        initConnection();
+        pattern = Pattern.compile(IPADDRESS_PATTERN);
+
+        //initConnection();
     }
-
-
 
     @Override
     protected void onDestroy() {
@@ -77,12 +94,30 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, MediaActivity.class));
     }
 
+    public boolean validate(final String ip){
+        matcher = pattern.matcher(ip);
+        return matcher.matches();
+    }
+
+    @OnClick(R.id.btn_connect)
+    public void connect() {
+        if(validate(ipEt.getText().toString())) {
+            broker = "tcp://" + ipEt.getText().toString() + ":1883"; // tcp: is necessary
+            Toast.makeText(this, "Connect to: " + broker, Toast.LENGTH_SHORT).show();
+            initConnection();
+        } else {
+            Toast.makeText(this, "Enter IP address", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void initConnection() {
         try {
             client = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
             client.connect(connOpts);
+
+            Toast.makeText(this, "connection: " + client.isConnected(), Toast.LENGTH_SHORT).show();
         } catch (MqttException e) {
             e.printStackTrace();
         }
